e24e7b58aca1719f24e1308854ace6defc6be731
hyeon924 <hyeonn4713@gmail.com>
2026-01-25 22:24:40 +0900

    project update

--- list/list01/script.js
+++ list/list01/script.js
@@ -1,73 +1,57 @@
-// moveType (0:left / 1:right) 
-var moveType = 0; 
-// 이동시간간격 3초 
-var moveSpeed = 3000; 
-// 움직이는 작업중 다시 명령 받지 않음 
-var moveWork = false; 
-// 일시정지 flag 
-var movePause = false; 
-function imgMove(){ 
-    if(moveWork==false){ 
-       // 0d\일경우 left방향 
-      if(moveType==0){ 
-         // 맨처음 이미지의 폭 
-         var aWidth = $(".RollDiv > div > a:first").width(); 
-         // 롤링마지막에 맨처음의 a태그 추가 
-         $(".RollDiv > div").append("<a href=\""+$(".RollDiv > div > a:first").attr("href")+"\">" + $(".RollDiv > div > a:first").html()+ "</a>"); 
-         // 맨처음이미지를 왼쪽으로 이동시킨다. 
-         $(".RollDiv > div > a:first").animate({marginLeft:-aWidth},{duration:moveSpeed,step:function(){ 
-         // 이동중 만약 일시정지 flag가 true라면 
-         if(movePause==true){ 
-            // 이동을 멈춘다 
-            $(this).stop(); 
-         } 
-         },complete:function(){ 
-         // 이동을 마친후 첫번째 a태그를 지워버린다 
-         $(this).remove(); 
-         // 이미지 움직이는것을 다시 실행 
-         imgMove(); 
-      }}); 
-      }else{ 
-      // 마지막 a태그의 폭 
-       var aWidth = $(".RollDiv > div > a:last").width(); 
-       // a태그 앞에 마지막의 a태그를 생성한다 단 스타일은 마지막 a태그의 폭만큼 빼준다 
-       $("<a href=\"" + $(".RollDiv > div > a:last").attr("href")+ "\" style=\"margin-left:-" + aWidth + "px\">" + $(".RollDiv > div > a:last").html()+ "</a>").insertBefore(".RollDiv > div > a:first") 
-       // 맨처음 a태그의 margin-left를 다시 0으로 맞춰준다. 
-      $(".RollDiv > div > a:first").animate({marginLeft:0},{duration:moveSpeed,step:function(){ 
-       // 이동중 만약 일시정지 flag가 true라면 
-       if(movePause==true){ 
-          // 이동을 멈춘다 
-          $(this).stop(); 
-       } 
-       },complete:function(){ 
-       // 이동을 마친후 마지막 a태그를 지워버린다 
-       $(".RollDiv > div > a:last").remove(); 
-       // 이미지 움직이는것을 다시 실행 
-       imgMove(); 
-    }}); 
- } 
- } 
- } 
- function goMove(){ 
-    // 일시정지가 풀려있을 경우를 대비하여 일시정지를 풀러준다 
-    movePause=false; 
-    // 0d\일경우 left방향 
-    if(moveType==0){ 
-       imgMove(); 
-       }else{ 
-       $(".RollDiv > div > a:first").animate({marginLeft:0},{duration:moveSpeed,step:function(){ 
-       // 이동중 만약 일시정지 flag가 true라면 
-       if(movePause==true){ 
-          // 이동을 멈춘다 
-          $(this).stop(); 
-      } 
-       },complete:function(){ 
-      // 이동을 마친후 마지막 a태그를 지워버린다 
-      //$(".RollDiv > div > a:last").remove(); 
-      // 이미지 움직이는것을 다시 실행 
-      imgMove(); 
-   }}); 
-}
-   
-}
-imgMove();
+/**
+ * 파트너 사이트 롤링 슬라이더
+ * 모던한 JavaScript로 재작성
+ */
+
+(function () {
+  "use strict";
+
+  document.addEventListener("DOMContentLoaded", function () {
+    initPartnerSlider();
+  });
+
+  /**
+   * 파트너 사이트 슬라이더 초기화
+   * CSS 애니메이션을 사용하되, JavaScript로 제어 가능하도록 구현
+   */
+  function initPartnerSlider() {
+    const sliderContainer = document.querySelector(".partner-slider-container");
+
+    if (!sliderContainer) return;
+
+    // 슬라이더가 중단되었는지 확인
+    let isPaused = false;
+
+    // 마우스 호버 시 일시정지
+    const partnerSection = document.querySelector(".partner-section");
+    if (partnerSection) {
+      partnerSection.addEventListener("mouseenter", function () {
+        isPaused = true;
+        sliderContainer.style.animationPlayState = "paused";
+      });
+
+      partnerSection.addEventListener("mouseleave", function () {
+        isPaused = false;
+        sliderContainer.style.animationPlayState = "running";
+      });
+    }
+
+    // 무한 루프를 위한 이미지 복제 (필요한 경우)
+    const images = sliderContainer.querySelectorAll("a");
+    if (images.length > 0) {
+      // CSS 애니메이션이 자동으로 처리하므로 추가 작업 불필요
+      // 필요시 여기에 추가 로직 구현
+    }
+  }
+
+  /**
+   * 레거시 jQuery 코드와의 호환성을 위한 함수들
+   * (기존 코드가 jQuery를 사용하는 경우를 대비)
+   */
+  if (typeof jQuery !== "undefined") {
+    // jQuery가 로드된 경우 기존 코드와의 호환성 유지
+    jQuery(document).ready(function ($) {
+      // 기존 코드가 필요한 경우 여기에 추가
+    });
+  }
+})();
